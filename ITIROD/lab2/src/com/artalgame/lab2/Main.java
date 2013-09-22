14508b936e8dfa01637764d61a0b002b56097158
artalgame <alkor1@yandex.ru>
2013-09-22 21:49:17 +0300

    add some changes to itirod lab2

--- ITIROD/lab2/src/com/artalgame/lab2/Main.java
+++ ITIROD/lab2/src/com/artalgame/lab2/Main.java
@@ -1,5 +1,8 @@
 package com.artalgame.lab2;
 
+import java.sql.Time;
+import java.util.Timer;
+
 public class Main {
 
 	/**
@@ -7,22 +10,22 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Matrix zeroLinkedMatrix = LinkedMatrix.getZeroMatrix(3);
+		Matrix zeroLinkedMatrix = LinkedMatrix.getZeroMatrix(4);
 		Matrix.drawMatrix(zeroLinkedMatrix, "Zero LinkedMatrix");
 		
-		Matrix zeroArrayMatrix = ArrayMatrix.getZeroMatrix(3);
+		Matrix zeroArrayMatrix = ArrayMatrix.getZeroMatrix(4);
 		Matrix.drawMatrix(zeroArrayMatrix, "Zero ArrayMatrix");
 		
-		Matrix identityLinkedMatrix = LinkedMatrix.getIdentityMatrix(3);
+		Matrix identityLinkedMatrix = LinkedMatrix.getIdentityMatrix(4);
 		Matrix.drawMatrix(identityLinkedMatrix, "Identity LinkedMatrix");
 		
-		Matrix identityArrayMatrix = ArrayMatrix.getIdentityMatrix(3);
+		Matrix identityArrayMatrix = ArrayMatrix.getIdentityMatrix(4);
 		Matrix.drawMatrix(identityArrayMatrix, "Identity ArrayMatrix");
 		
-		Matrix randomLinkedMatrix = LinkedMatrix.getRandomMatrix(4, 3);
+		Matrix randomLinkedMatrix = LinkedMatrix.getRandomMatrix(4, 4);
 		Matrix.drawMatrix(randomLinkedMatrix, "random LinkedMatrix");
 		
-		Matrix randomArrayMatrix = ArrayMatrix.getRandomMatrix(4, 3);
+		Matrix randomArrayMatrix = ArrayMatrix.getRandomMatrix(4, 4);
 		Matrix.drawMatrix(randomArrayMatrix, "random ArrayMatrix");
 		
 		Matrix res1 = randomLinkedMatrix.multiplyBy(identityArrayMatrix);
@@ -37,11 +40,25 @@ public class Main {
 		Matrix res4 = randomArrayMatrix.multiplyBy(zeroLinkedMatrix);
 		Matrix.drawMatrix(res4, "res4");
 		
-		Matrix random1 = LinkedMatrix.getRandomMatrix(100, 100);
-		Matrix random2 = ArrayMatrix.getRandomMatrix(100, 100);
 		
-		Matrix res = random1.multiplyBy(random2);
-
+		Matrix random1 = LinkedMatrix.getRandomMatrix(100, 100);
+		Matrix random2 = LinkedMatrix.getRandomMatrix(100, 100);
+		
+		long startTime = System.nanoTime();
+		random1.multiplyBy(random2);
+		long endTime = System.nanoTime();
+		long duration = endTime - startTime;
+		System.out.println("LinkedMatrixes multiply duration have been " + duration/1000000000 +"."+ duration%1000000000 + "s");
+		
+		random1 = ArrayMatrix.getRandomMatrix(100, 100);
+		random2 = ArrayMatrix.getRandomMatrix(100, 100);
+		
+		startTime = System.nanoTime();
+		random1.multiplyBy(random2);
+		endTime = System.nanoTime();
+		duration = endTime - startTime;
+		System.out.println("ArrayMatrixes multiply duration have been " + duration/1000000000 +"."+ duration%1000000000 + "s");
+		
 	}
 
 }
